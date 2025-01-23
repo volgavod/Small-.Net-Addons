702337c76d4f777030c5aa20cd6b98fa49761c4c
Артём / Artem <86970970+volgavod@users.noreply.github.com>
2025-01-24 00:44:01 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Small .Net Addons
+ 
---
+++ Small .Net Addons/Collections/LinkedList.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+namespace SmallDotNetAddons.Collections;
+
+/*
+	Exceptions
+	summary
+	Async
+*/
+
+public class LinkedList<T> : ICollection<T>, IDisposable
+{
+	private int _count = 0;
+	private Node<T>? _head;
+	private Node<T>? _tail;
+	public int Count => _count;
+	bool ICollection<T>.IsReadOnly => false;
+	public T this[int index]
+	{
+		get => GetByIndex(index).Item;
+		set => GetByIndex(index).Item = value;
+	}
+
+	public LinkedList() { }
+	public LinkedList(IEnumerable<T> items)
+	{
+		Insert(0, items);
+	}
+
+	void ICollection<T>.Add(T item)
+	{
+		AddLast(item);
+	}
+	public void AddFirst(T item)
+	{
+		Insert(0, item);
+	}
+	public void AddLast(T item)
+	{
+		Insert(_count, item);
+	}
+	public void Clear()
+	{
+		_head = null;
+		_tail = null;
+		_count = 0;
+	}
+	public bool Contains(T item)
+	{
+		foreach (T toFind in this)
+			if (toFind!.Equals(item))
+				return true;
+		return false;
+	}
+	public void CopyTo(T[] array, int arrayIndex)
+	{
+		ArgumentNullException.ThrowIfNull(array);
+		ArgumentOutOfRangeException.ThrowIfNegative(arrayIndex);
+		if (array.Length - arrayIndex < _count)
+			throw new ArgumentException();
+
+		int i = arrayIndex;
+		foreach (T item in this)
+		{
+			array[i + arrayIndex] = item;
+			i++;
+		}
+	}
+	public void Dispose()
+	{
+		Clear();
+		GC.SuppressFinalize(this);
+	}
+	public IEnumerator<T> GetEnumerator()
+	{
+		return new Enumerator(_head);
+	}
+	IEnumerator IEnumerable.GetEnumerator()
+	{
+		return GetEnumerator();
+	}
+	private Node<T> GetByIndex(int index)
+	{
+		if (index < 0 || index >= _count)
+			throw new IndexOutOfRangeException();
+
+		Node<T>? current = _head;
+		for (int i = 0; i < index; i++)
+		{
+			if (current?.Next == null && i == index - 1)
+				throw new IndexOutOfRangeException();
+			current = current?.Next;
+		}
+		return current!;
+	}
+	public void Insert(int index, T item)
+	{
+		Node<T> node = new(item);
+		Insert(index, new NodeSequence(node, node, 1));
+	}
+	public void Insert(int index, IEnumerable<T> items)
+	{
+		if (!items.Any()) return;
+		Insert(index, NodeSequence.Create(items));
+	}
+	private void Insert(int index, NodeSequence sequence)
+	{
+		if (index < 0 || index > _count)
+			throw new IndexOutOfRangeException();
+
+		if (index == 0)
+		{
+			sequence.Tail.Next = _head;
+			_head = sequence.Head;
+			if (_count == 0)
+				_tail = sequence.Tail;
+		}
+		else if (index == _count)
+		{
+			_tail!.Next = sequence.Head;
+			_tail = sequence.Tail;
+		}
+		else
+		{
+			Node<T> current = _head!;
+			for (int i = 0; i < index - 1; i++)
+				current = current.Next!;
+			sequence.Tail.Next = current.Next;
+			current.Next = sequence.Head;
+		}
+
+		_count += sequence.Count;
+	}
+	public bool Remove(T item)
+	{
+		if (_head == null || item == null)
+			return false;
+		if (item.Equals(_head))
+		{
+			_head = _head?.Next;
+			return true;
+		}
+		Node<T> previous = _head;
+		Node<T> current = _head.Next!;
+		for (int i = 1; i < _count; i++)
+		{
+			if (item.Equals(current.Item))
+				return RemoveNext(previous);
+			previous = current;
+			current = current.Next!;
+
+		}
+		return false;
+	}
+	public bool RemoveAt(int index)
+	{
+		if (index < 0 || index >= _count || _count == 0)
+			return false;
+		if (_count == 1)
+		{
+			_head = null;
+			_tail = null;
+		}
+		else if (index == 0)
+		{
+			_head = _head?.Next;
+			//Node<T> second = _head!.Next!;
+			//_head.Next = null;
+			//_head = second;
+		}
+		else
+		{
+			Node<T> previous = GetByIndex(index - 1);
+			return RemoveNext(previous);
+		}
+		_count--;
+		return true;
+	}
+	private bool RemoveNext(Node<T> previous)
+	{
+		if (previous.Next == null)
+			return false;
+		Node<T> removable = previous.Next;
+		if (removable.Equals(_tail))
+		{
+			previous.Next = null;
+			_tail = previous;
+		}
+		else
+		{
+			previous.Next = removable.Next;
+			removable.Next = null;
+		}
+		_count--;
+		return true;
+	}
+
+	internal class Node<I>(I item)
+	{
+		public I Item = item;
+		public Node<I>? Next;
+	}
+	private record NodeSequence(Node<T> Head, Node<T> Tail, int Count)
+	{
+		public static NodeSequence Create(IEnumerable<T> items)
+		{
+			if (!items.Any())
+				throw new Exception();
+			using LinkedList<T> list = new();
+			foreach (T item in items)
+				list.AddLast(item);
+			return new NodeSequence(list._head!, list._tail!, list._count);
+		}
+	}
+	public struct Enumerator : IEnumerator<T>
+	{
+		private Node<T>? _current;
+		private Node<T>? _head;
+		public T Current => _current.Item;
+		object IEnumerator.Current => _current.Item;
+
+		internal Enumerator(Node<T> head)
+		{
+			_head = head;
+		}
+		public void Dispose()
+		{
+			_head = null;
+			_current = null;
+			GC.SuppressFinalize(this);
+		}
+		public bool MoveNext()
+		{
+			if (_current == null)
+			{
+				_current = _head;
+				return _head != null;
+			}
+			if (_current.Next != null)
+			{
+				_current = _current.Next;
+				return true;
+			}
+			return false;
+		}
+		public void Reset()
+		{
+			_current = null;
+		}
+	}
+}
